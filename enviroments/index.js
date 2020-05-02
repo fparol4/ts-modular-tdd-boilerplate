19e6e6894636cc13a6d050291d776c9a194d0f34
Fabricio Parola <fabricio.parola@alicerceedu.com.br>
2020-05-02 14:25:47 -0300

    chore: add ts and ts-node

--- enviroments/index.js
+++ enviroments/index.js
@@ -1,19 +1,19 @@
-import Dotenv from 'dotenv'
-import Path from 'path'
+import Dotenv from "dotenv";
+import Path from "path";
 
 class Enviroment {
   static enviroments = {
-    dev: './.env.development',
-    prod: './.env.production',
-    test: './.env.test',
-  }
+    dev: "./.env.development",
+    prod: "./.env.production",
+    test: "./.env.test",
+  };
 
   static getEnvPath(nodeEnv) {
-    const envFileName = enviroments[nodeEnv] || enviroments.dev
-    return Path.resolve(__dirname, envFileName)
+    const envFileName = enviroments[nodeEnv] || enviroments.dev;
+    return Path.resolve(__dirname, envFileName);
   }
 }
 
 export default Dotenv.config({
   path: Enviroment.getEnvPath(process.env.NODE_ENV),
-})
+});
